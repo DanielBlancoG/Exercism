bef28d53d28896e3195ebaa04b1b9349c9f39d56
Daniel Blanco <danielbg156@gmail.com>
2025-09-02 21:29:20 +0200

    Refactor funciones de estado de cocción y tiempo de preparación, y agregar estructura para calcular ingredientes

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -2,14 +2,34 @@
 // @ts-check
 
 /* Determine whether the lasagne is done */
-export function cookingStatus (remainingTime){
-    return remainingTime === 0 || remainingTime === !Number ? 'Lasagna is done.' : remainingTime > 0 ? 'Not done, please wait.' : 'You forgot to set the timer.'
+export function cookingStatus(remainingTime) {
+  return remainingTime === 0 || remainingTime === !Number
+    ? "Lasagna is done."
+    : remainingTime > 0
+    ? "Not done, please wait."
+    : "You forgot to set the timer.";
 }
 
 /* Estimate the preparation time */
-export function preparationTime(layers, timePerLayer){
-    return layers.length * (timePerLayer === undefined ? 2 : timePerLayer);
+export function preparationTime(layers, timePerLayer) {
+  return layers.length * (timePerLayer === undefined ? 2 : timePerLayer);
 }
 
+/* Calculate the amount of sauce and noodles needed */
+export function quantities(ingredients) {
+  const count = ingredients.reduce((acc, ingredient) => {
+    if (ingredient === "noodles") acc.noodles += 50;
+    if (ingredient === "sauce") acc.sauce += 0.2;
+    return acc;
+  }, {
+    noodles : 0,
+    sauce: 0
+  });
 
+  return count;
+}
 
+/* Add the secret ingredient */
+export function addSecretIngredient(){
+    
+}
