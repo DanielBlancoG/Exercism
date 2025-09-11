b4fde0135bd698403ed81ca6633b4e706c348cb0
Daniel Blanco <danielbg156@gmail.com>
2025-09-11 18:52:44 +0200

    Feat: Add 3 funcion
    Class & Prototypes

---
+++ javascript/windowing-system/windowing-system.js
@@ -0,0 +1,39 @@
+// @ts-check
+
+/**
+ * Implement the classes etc. that are needed to solve the
+ * exercise in this file. Do not forget to export the entities
+ * you defined so they are available for the tests.
+ */
+
+/* 1. Define Size for storing the dimensions of the window */
+export function Size(width = 80, height = 60) {
+  this.width = width;
+  this.height = height;
+}
+
+Size.prototype.resize = function (newWidth, newHeight) {
+  this.width = newWidth;
+  this.height = newHeight;
+};
+
+/* 2. Define Position to store a window position */
+export function Position (x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+}
+
+Position.prototype.move = function (newX, newY){
+    this.x = newX;
+    this.y = newY;
+}
+
+/* 3. Define a ProgramWindow class */
+export class ProgramWindow {
+    constructor(){
+        this.screenSize = new Size(800, 600);
+        this.size = new Size();
+        this.position = new Position();
+    }
+}
+
