9204ad8aa089c44807b7f229a9d019d7e262a134
Daniel Blanco <danielbg156@gmail.com>
2025-09-12 15:08:08 +0200

    Feat: add 2 methods and 1 functions
    It was implemented encapsulation and rehused the methods to simplify the code

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -18,22 +18,56 @@ Size.prototype.resize = function (newWidth, newHeight) {
 };
 
 /* 2. Define Position to store a window position */
-export function Position (x = 0, y = 0) {
-    this.x = x;
-    this.y = y;
+export function Position(x = 0, y = 0) {
+  this.x = x;
+  this.y = y;
 }
 
-Position.prototype.move = function (newX, newY){
-    this.x = newX;
-    this.y = newY;
-}
+Position.prototype.move = function (newX, newY) {
+  this.x = newX;
+  this.y = newY;
+};
 
-/* 3. Define a ProgramWindow class */
+/* 3-4-5. Define a ProgramWindow class 
+&& Add a method to resize the window
+&& Add a method to move the window*/
 export class ProgramWindow {
-    constructor(){
-        this.screenSize = new Size(800, 600);
-        this.size = new Size();
-        this.position = new Position();
-    }
+  constructor() {
+    this.screenSize = new Size(800, 600);
+    this.size = new Size();
+    this.position = new Position();
+  }
+
+  resize(newSize) {
+    this.size.resize(
+      Math.max(
+        1,
+        Math.min(newSize.width, this.screenSize.width - this.position.x)
+      ),
+      Math.max(
+        1,
+        Math.min(newSize.height, this.screenSize.height - this.position.y)
+      )
+    );
+  }
+
+  move(newPosition) {
+    this.position.move(
+      Math.max(
+        0,
+        Math.min(newPosition.x, this.screenSize.width - this.size.width)
+      ),
+      Math.max(
+        0,
+        Math.min(newPosition.y, this.screenSize.height - this.size.height)
+      )
+    );
+  }
 }
 
+/* 6. Change a program window */
+export function changeWindow(programWindow) {
+  programWindow.resize(new Size(400, 300));
+  programWindow.move(new Position(100, 150));
+  return programWindow;
+}
