5168901a7e6bbbb75bf2204342a71992b27f57dd
Daniel Blanco <danielbg156@gmail.com>
2025-09-28 15:43:15 +0200

    Add 1 function
    sum a gigasecond to entry date

---
+++ javascript/gigasecond/gigasecond.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const gigasecond = (date) => {
+  const new_date = date.getTime() + (1e9 * 1000);/* Conversion gigasegundo a milisegundos */
+  return new Date (new_date)
+};
+
